cde1ad02d872c42d2eb0eaf4485482410b24c86b
Brandon Clair <brandon-clair@users.noreply.github.com>
2020-09-11 13:14:48 -0700

    Bump Version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='resizerful',
-      version='0.2',
+      version='0.3',
       description='Serverless image handler wrapper',
       url='git@github.com:wndrfl/resizerful.git',
       author='Manuel Aguilera',
